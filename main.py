f55411ab49d66a3723f744baa7800d60abd7c42b
MarchiMatheus <diniz.marchi@gmail.com>
2019-10-22 22:05:47 -0300

    Adding functionality to the article class

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import re
+
 from src.Article import Article
 
 # Process an article
 def process():
     
-    filePath = './files/pt/o_lobo_e_a_lua.txt'
+    fileNamePatern = '.*\/(.*).txt'
+    filePath = './files/pt/o_lobo_e_a_lua.txt'    
 
     article = Article(filePath)
-    print(article.getDictionary())
+    total = article.getTotalLetters()
+    vowels = article.getVowelQuantity()
+    consonants = article.getConsonantQuantity()
+
+    print('\n###########################################################\n')
+    print('Processed numbers for file: ' + re.match(fileNamePatern, filePath).group(1) + '\n')
+
+    print('Vowels: ' + str(vowels) + ', Percentage: %.2f' %((vowels/total)*100) + '%')
+    print('Consonants: ' + str(consonants) + ', Percentage: %.2f' %((consonants/total)*100) + '%')
+    
+    print('\nTotal letters: ' + str(total))
+
+    print('\n###########################################################\n')
 
 # Execute main function
 if __name__ == '__main__':
--- src/Article.py
+++ src/Article.py
@@ -1,4 +1,3 @@
-
 class Article():
     """ This class processes the file passed in the constructor and executes some operations """
 
@@ -16,24 +15,89 @@ class Article():
         filePath : Path to the file that will be processed
         """
 
-        self.letterDictionary = {}
+        # Initialize Article class private variables
+        self.__letterDictionary = {}
+        self.__vowelArray = ['a', 'e', 'i', 'o', 'u', 'á', 'à', 'â', 'ã', 'é', 'ê', 'í', 'ó', 'õ', 'ú']
 
         with open(filePath, encoding="utf-8") as file:
 
             for line in file:
                 for letter in line:
 
-                    if letter in self.letterDictionary.keys():
-                        self.letterDictionary[letter] += 1
+                    # Check if letter variable is actually a letter
+                    if letter.isalpha():
+
+                        # Normalize the letter
+                        lowerLetter = letter.lower()
+
+                        if lowerLetter in self.__letterDictionary.keys():
+                            self.__letterDictionary[lowerLetter] += 1
                     
-                    else:
-                        self.letterDictionary[letter] = 1
+                        else:
+                            self.__letterDictionary[lowerLetter] = 1
 
     
     def getDictionary(self):
 
         """
         Returns the letter dictionary
+
+        Parameters
+        ----------
+        self : Article class object
         """
 
-        return self.letterDictionary
+        return self.__letterDictionary
+
+    def getTotalLetters(self):
+
+        """
+        Returns the total amount of letters in the article
+
+        Parameters
+        ----------
+        self : Article class object
+        """
+
+        totalLetters = 0
+
+        for letter in self.__letterDictionary:
+            totalLetters += self.__letterDictionary[letter]
+
+        return totalLetters
+
+    def getVowelQuantity(self):
+
+        """
+        Returns the quantity of vowels in the article
+
+        Parameters
+        ----------
+        self : Article class object
+        """
+
+        totalVowels = 0
+
+        for vowel in self.__vowelArray:
+            if vowel in self.__letterDictionary.keys():
+                totalVowels += self.__letterDictionary[vowel]
+
+        return totalVowels
+
+    def getConsonantQuantity(self):
+
+        """
+        Returns the quantity of consonants in the article
+
+        Parameters
+        ----------
+        self : Article class object
+        """
+
+        totalConsonants = 0
+
+        for letter in self.__letterDictionary:
+            if letter not in self.__vowelArray:
+                totalConsonants += self.__letterDictionary[letter]
+
+        return totalConsonants
